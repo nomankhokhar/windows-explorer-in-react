fede9b7681e5b75c2f8b4de371f11c5f795f6b5b
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2023-11-18 13:52:04 +0500

    Successfully Able to Print Parent fromt he JOSN

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Folder from "./components/Folder";
-import explorer from "./data/FolderData";
 import useTraverseTree from "./hooks/use-traverse-tree";
 import data from "./argocdTree.json";
 
 function App() {
-  const [explorerData, setExplorerData] = useState(explorer);
+  const [explorerData, setExplorerData] = useState({
+    id: "1",
+    parentID: "1",
+    name: "guestbook",
+    isFolder: true,
+    items: [],
+  });
 
   const { insertNode, deleteNode } = useTraverseTree();
 
@@ -18,33 +23,31 @@ function App() {
       item,
       isFolder
     );
-    setExplorerData(finalTree);
+    setExplorerData((preFinalTree) => (preFinalTree = finalTree));
   };
 
   const [childs, setChilds] = useState([]);
   const [parents, setParents] = useState([]);
 
-  useEffect(() => {
-    const handleNodes = () => {
-      data?.nodes?.forEach((node) => {
-        if (node.hasOwnProperty("parentRefs")) {
-          setChilds((prevChilds) => [...prevChilds, node]);
-        } else {
-          const newNode = { ...node, uid: new Date().getTime(), parentID: "1" };
-          handleInsertNode(newNode.parentID, newNode.uid, newNode.name, true);
-        }
-      });
-    };
-
-    handleNodes();
-
-    return () => {};
-  }, []);
+  const handleNodes = async () => {
+    data?.nodes?.forEach((node) => {
+      if (node.hasOwnProperty("parentRefs")) {
+        setChilds((prevChilds) => [...prevChilds, node]);
+      } else {
+        const newNode = { ...node, uid: new Date().getTime(), parentID: "1" };
+        handleInsertNode(newNode.parentID, newNode.uid, newNode.name, true);
+      }
+    });
+  };
 
   const handleDeleteNode = (id) => {
     const finalTree = deleteNode(explorerData, id);
     setExplorerData(finalTree);
   };
+
+  useEffect(() => {
+    handleNodes();
+  }, []);
 
   // useEffect(() => {
   //   const updatedExplorer = mergeDataIntoExplorer(explorer, data.nodes);
